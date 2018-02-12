dc8540e4f86922c7c731b8ad3ead7cd8248e1480
timvisee <timvisee@gmail.com>
2018-02-12 21:31:03 +0100

    Initial commit, spawn the external pixelpwnr renderer

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate pixelpwnr_render;
+
+use pixelpwnr_render::color::Color;
+use pixelpwnr_render::pixmap::Pixmap;
+use pixelpwnr_render::renderer::Renderer;
+
+fn main() {
+    // Build a pixelmap
+    let mut pixmap = Pixmap::new(800, 600);
+    pixmap.set_pixel(10, 10, Color::from_rgb(255, 0, 0));
+
+    // Build and run the renderer
+    let mut renderer = Renderer::new(&pixmap);
+    renderer.run();
+}
