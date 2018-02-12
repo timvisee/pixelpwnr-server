6250b83858245b10b144196f71517432939ef5bc
timvisee <timvisee@gmail.com>
2018-02-13 00:05:12 +0100

    Use 4 spaces to indent

--- src/main.rs
+++ src/main.rs
@@ -12,43 +12,43 @@ use pixelpwnr_render::Renderer;
 /// Main application entrypoint.
 fn main() {
     // Set up a listener for the server
-	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Listening on 127.0.0.1:8080...");
 
-	// Accept connections and process them, spawning a new thread for each one
-	for stream in listener.incoming() {
-		match stream {
-			Ok(stream) => {
-				thread::spawn(move || handle_client(stream));
-			},
-			Err(_) => {}, // Connection failed
-		}
-	}
+    // Accept connections and process them, spawning a new thread for each one
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || handle_client(stream));
+            },
+            Err(_) => {}, // Connection failed
+        }
+    }
 
     // Render the pixelflut screen
-	render();
+    render();
 }
 
 /// Handle a client connection.
 fn handle_client(stream: TcpStream) {
     // Create a buffered reader
-	let mut reader = BufReader::new(stream);
+    let mut reader = BufReader::new(stream);
 
     // A client has connected
     println!("A client has connected");
 
     // Read client input
-	loop {
+    loop {
         // Read a new line
         let mut data = String::new();
-		if let Err(_) = reader.read_line(&mut data) {
+        if let Err(_) = reader.read_line(&mut data) {
             println!("An error occurred, closing stream");
             return;
         }
 
         // Show the received data
         println!("Received: {}", data.trim());
-	}
+    }
 }
 
 fn render() {
