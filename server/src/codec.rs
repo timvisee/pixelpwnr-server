3494bbcfc739a3c5ab8590540c6c70c5fa8be38e
datdenkikniet <jcdra1@gmail.com>
2022-08-18 18:52:16 +0200

    server: Add basic tests to codec

--- server/src/codec.rs
+++ server/src/codec.rs
@@ -289,6 +289,7 @@ where
                 CmdResult::ClientErr(err) => {
                     // Report the error to the client
                     self.buffer(&format!("ERR {}\r\n", err).as_bytes());
+                    let _ = self.poll_flush(cx);
                     break Err("Client error".to_string());
                 }
 
@@ -296,6 +297,9 @@ where
                 CmdResult::ServerErr(err) => {
                     // Show an error message in the console
                     println!("Client error \"{}\" occurred, disconnecting...", err);
+
+                    self.buffer(b"ERR Line length >1024\r\n");
+                    let _ = self.poll_flush(cx);
 
                     // Disconnect the client
                     break Err(format!("Client error occured. {}", err));
@@ -323,4 +327,35 @@ where
             Err(m) => Poll::Ready(m),
         }
     }
+}
+
+#[tokio::test]
+async fn response_newline() {
+    let stats = Arc::new(Stats::new());
+    let pixmap = Arc::new(Pixmap::new(400, 800));
+
+    let mut test = tokio_test::io::Builder::new()
+        // Test all commands that require a response
+        .read(b"PX 16 16\r\n")
+        .write(b"PX 16 16 000000\r\n")
+        .read(b"SIZE\r\n")
+        .write(b"SIZE 400 800\r\n")
+        .read(b"HELP\r\n")
+        .write(format!("{}\r\n", Cmd::help_list()).as_bytes())
+        // Test different variations of newlines
+        .read(b"PX 16 16\n")
+        .write(b"PX 16 16 000000\r\n")
+        // Verify that adding a few whitespaces after the command doesn't make a difference
+        .read(b"PX 16 16                     \n")
+        .write(b"PX 16 16 000000\r\n")
+        // Using an out of bounds index should return an error
+        .read(b"PX 1000 0\r\n")
+        .write(b"ERR x coordinate out of bound\r\n")
+        .build();
+
+    let test = Pin::new(&mut test);
+
+    let lines = Lines::new(test, stats.clone(), pixmap.clone());
+
+    lines.await;
 }
